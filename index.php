1e32c3ebd20c7966eeb4a8e3deca4f42352838c0
ferbarcante <barcantef@gmail.com>
2024-09-18 19:59:20 -0300

    introduction

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+    echo "I love pizza! <br>";
+    echo "It's really good";
+    //this is a comment
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order a pizza</button>
+</body>
+</html>
