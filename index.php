a6903d040a81d8eb418626bd98c1d01d7a3dd1bf
ferbarcante <barcantef@gmail.com>
2024-09-18 20:18:12 -0300

    variaveis

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <?php 
-    echo "I love pizza! <br>";
-    echo "It's really good";
-    //this is a comment
+    //declaring a variable
+    $name = "Maria Fernanda";
+    $food = "pizza";
+    $email = "maria@gmail.com";
 
+    $age = 21;
+    $year = date("Y");
+    $price = 15.20;
+
+    $employed = true;
+
+    //output
+    echo "Hello {$name}";
+    echo "<br> You like {$food}";
+    echo "<br> Your email is {$email}";
+    echo "<br> Your age is {$age}";
+    echo "<br> The year is {$year}";
+    echo "<br> The price of the pizza is \${$price}";
+    if($employed=true){
+        echo"<br>You are employed!";
+    }
+
+    $total = null;
+    $total = 3 * 2;
+    echo"<br>3 x 2 = {$total}";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <br>
-    <button>order a pizza</button>
-</body>
-</html>
