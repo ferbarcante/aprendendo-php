fc9b6c5d74e529c00f7df77877c885fe80565a11
ferbarcante <barcantef@gmail.com>
2024-09-18 23:17:15 -0300

    for loop

--- index.php
+++ index.php
@@ -2,16 +2,7 @@
 
 <?php
     
-    $age = 0;
-
-    if($age >=18){
-        echo"You may enter this site";
-    } else if($age == 0){
-        echo "You were just born";
+    for($i = 0; $i < 4; $i++){
+        echo"i = {$i} <br>";
     }
-    else {
-        echo "You must be 18+ to enter";
-    }
-
-
 ?>
